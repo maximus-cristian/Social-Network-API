bcf096eb62b41fce046c333e61dfaa1d8dc7c9d0
Maximus Cristian <mcristia2938@gmail.com>
2022-08-19 00:52:45 -0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,15 @@
 const User = require('../models/User');
 
 module.exports = {
-//GET all users
+
   getUsers(req, res) {
     User.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-//GET single user by _id and populate thought/friend data
+
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-      // .select('-__v')
-      // .populate('thoughts', 'friends')
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
@@ -19,13 +17,11 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  //POST create a new user
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
-//PUT update USER by _id 
   updateUser(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -42,7 +38,6 @@ module.exports = {
         res.status(500).json(err);
       });
   },
-//DELETE USER by _id
   deleteUser(req, res) {
     User.findOneAndRemove({ _id: req.params.userId })
       .then((user) =>
@@ -52,7 +47,6 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-//POST add a new friend to Users friends list.
   addUserFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -66,7 +60,6 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-// DELETE remove friend from Users friends list. 
   removeUserFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
