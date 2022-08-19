c48c07147e6942da48b3de15162475033dd1f8eb
Maximus Cristian <mcristia2938@gmail.com>
2022-08-19 00:53:40 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,1 @@
 const { connect, connection } = require('mongoose');
-
-// Wrap Mongoose around local connection to MongoDB
-connect('mongodb://127.0.0.1:27017/HelloFriendsDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Export connection
-module.exports = connection;
